e385acdc01909c462eee31798780e99aa1e24b85
Vikalp Malhotra <kimboho@gmail.com>
2021-12-02 11:27:43 -0500

    finished

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.splice(0, 1);
+}
+
+function appendCat(name) {
+    let newcats = cats.slice();
+    newcats.push(name);
+    return newcats;
+}
+
+function prependCat(name) {
+    let newcats = cats.slice();
+    newcats.unshift(name);
+    return newcats;
+}
+
+function removeLastCat() {
+    let newcats = cats.slice();
+    newcats.pop();
+    return newcats;
+
+}
+
+function removeFirstCat() {
+    let newcats = cats.slice();
+    newcats.shift();
+    return newcats;
+
+}
+
+
